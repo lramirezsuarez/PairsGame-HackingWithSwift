efed1ae3b747a143ca9fb873518c987a2b75bc75
Luis Alejandro Ramirez <lramirezsuarez@gmail.com>
2020-04-15 10:09:33 -0500

    Initial Commit

---
+++ SwiftUILiveCoding/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUILiveCoding
+//
+//  Created by Luis Ramirez on 15/04/20.
+//  Copyright © 2020 Lramirez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
